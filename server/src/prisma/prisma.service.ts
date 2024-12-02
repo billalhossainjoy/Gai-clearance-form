14d214a90286780731532c65c3a3aed73fcb1a5e
billalhossainjoy <billalhossain.bhj@gmail.com>
2024-12-02 06:13:30 +0600

    next

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -6,10 +6,16 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super();
+  }
+  
   async onModuleInit() {
     await this.$connect();
   }
   async onModuleDestroy() {
     await this.$disconnect();
   }
+
+
 }
