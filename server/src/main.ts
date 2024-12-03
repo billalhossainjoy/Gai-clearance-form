80c618f104ce4901bd9247aa90a3b35fe31aa45b
billalhossainjoy <billalhossain.bhj@gmail.com>
2024-12-03 20:34:44 +0600

    update client url

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.use(
     cors({
       origin: [
-        'https://gai-clearance-form.vercel.app',
+        'https://https://gai-clearance-form-client.vercel.app/',
         'http://localhost:5173',
       ],
       credentials: true,
