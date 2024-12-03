b323bea4aca6542d6a1d8464c15f69f9cded8d8c
billalhossainjoy <billalhossain.bhj@gmail.com>
2024-12-03 20:39:45 +0600

    update api client

--- client/src/lib/apiClient.tsx
+++ client/src/lib/apiClient.tsx
@@ -3,10 +3,6 @@ import axios from "axios";
 const ApiClient = axios.create({
   baseURL: import.meta.env.VITE_REST_API,
   withCredentials: true,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
 });
 
 export default ApiClient;
