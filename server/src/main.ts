3b0de19c4f6af8c0bdde96ab952adadefddac3f0
billalhossainjoy <billalhossain.bhj@gmail.com>
2024-12-03 20:02:57 +0600

    update method

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
         'http://localhost:5173',
       ],
       credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
