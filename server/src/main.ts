a63cb2c75910ca23aac16c8e84868b6ed93ebaae
billalhossainjoy <billalhossain.bhj@gmail.com>
2024-12-03 20:37:49 +0600

    complete

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
         'http://localhost:5173',
       ],
       credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
